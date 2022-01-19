08710420c7564a425c5037c5b18db8eea28b9cd7
jungminji <wjdalswl0215@gmail.com>
2022-01-19 10:12:31 +0900

    InputStreamReader

---
+++ src/basic/io/StreamEx02.java
@@ -0,0 +1,19 @@
+package basic.io;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class StreamEx02 {
+    public static void main(String[] args) {
+        InputStream in = System.in;
+        InputStreamReader ir = new InputStreamReader(in); // 자동 길이 조절 안됨
+
+        try {
+            char[] data = new char[3];
+            ir.read(data);
+            System.out.println(data);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
