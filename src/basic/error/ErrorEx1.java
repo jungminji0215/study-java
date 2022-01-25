c0f4b6259ee846c5d002ce40cb84e619ed280c4c
jungminji <wjdalswl0215@gmail.com>
2022-01-25 10:07:55 +0900

    예외 실습
    예외를 고의로 만들어서
    결과를 봐 보았다.

---
+++ src/basic/error/ErrorEx1.java
@@ -0,0 +1,15 @@
+package basic.error;
+
+public class ErrorEx1 {
+    public static void main(String[] args) {
+        try {
+            Exception e = new Exception("예외 발생!!");
+            throw e;
+            
+        }catch (Exception e){
+            System.out.println("에러 메세지 " + e.getMessage());
+            e.printStackTrace();
+        }
+        System.out.println("프로그램 정상 종료");
+    }
+}
