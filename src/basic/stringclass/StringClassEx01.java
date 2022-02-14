3cd93c9cea154d940f73f5b63e9683ea6de9ff4f
jungminji <wjdalswl0215@gmail.com>
2022-02-14 22:01:03 +0900

    String 클래스
    - String 클래스 테스트
    - == 비교

---
+++ src/basic/stringclass/StringClassEx01.java
@@ -0,0 +1,15 @@
+package basic.stringclass;
+
+public class StringClassEx01 {
+    public static void main(String[] args) {
+        String a = "1";
+        String b = "1";
+
+        System.out.println(a);
+        System.out.println(b);
+
+        if (a == b){
+            System.out.println("주소 같음");
+        }
+    }
+}
