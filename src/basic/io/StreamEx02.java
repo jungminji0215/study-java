f9f6241fc931ad6978bb4953244daae694240830
jungminji <wjdalswl0215@gmail.com>
2022-02-21 04:05:30 +0900

    stream

--- src/basic/io/StreamEx02.java
+++ src/basic/io/StreamEx02.java
@@ -3,13 +3,14 @@ package basic.io;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-public class StreamEx02 {
+public class   StreamEx02 {
     public static void main(String[] args) {
         InputStream in = System.in;
         InputStreamReader ir = new InputStreamReader(in); // 자동 길이 조절 안됨
 
         try {
             char[] data = new char[3];
+            System.out.println("ㅋㅋㅋㅋ");
             ir.read(data);
             System.out.println(data);
         }catch (Exception e){
