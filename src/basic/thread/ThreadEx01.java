8d882422e63ca6e52143760947d2e28ca8e7050b
jungminji <wjdalswl0215@gmail.com>
2022-03-05 03:08:17 +0900

    쓰레드 연습

---
+++ src/basic/thread/ThreadEx01.java
@@ -0,0 +1,29 @@
+package basic.thread;
+
+class ThreadEx01 {
+    public static void main(String args[]) {
+        ThreadEx1_1 t1 = new ThreadEx1_1();
+
+        Runnable r  = new ThreadEx1_2();
+        Thread   t2 = new Thread(r);
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class ThreadEx1_1 extends Thread {
+    public void run() {
+        for(int i=0; i < 500; i++) {
+            System.out.print(0);
+        }
+    }
+}
+
+class ThreadEx1_2 implements Runnable {
+    public void run() {
+        for(int i=0; i < 500; i++) {
+            System.out.print(1);
+        }
+    }
+}
