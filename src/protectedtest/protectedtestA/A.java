6702e9c426e1c8e2153d640527fdeac775cefcc1
jungminji <wjdalswl0215@gmail.com>
2023-11-05 03:37:30 +0900

    필요없는 파일 삭제

---
+++ src/protectedtest/protectedtestA/A.java
@@ -0,0 +1,18 @@
+package protectedtest.protectedtestA;
+
+public class A {
+    public String free;
+    protected String noFree;
+
+    public A(){
+
+    }
+
+    public String getFree() {
+        return free;
+    }
+
+    public String getNoFree() {
+        return noFree;
+    }
+}
---
+++ src/protectedtest/protectedtestB/B.java
@@ -0,0 +1,12 @@
+package protectedtest.protectedtestB;
+
+import protectedtest.protectedtestA.A;
+
+public class B {
+
+    public void method(){
+        A a = new A();
+        a.free = "123";
+
+    }
+}
